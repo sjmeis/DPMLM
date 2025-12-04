a53bb26f608e8f9bb6aa3c50fcf126e6b2077651
Stephen Meisenbacher <sjmeis@gtgd.com>
2025-12-04 07:37:35 +0100

    account for empty vectors

--- DPMLM.py
+++ DPMLM.py
@@ -489,6 +489,9 @@ class DPMLM():
             current = "{}_{}".format(targets[i], n[i])
 
             mask_logits = outputs[i][masked_position[i]].squeeze().detach().cpu().numpy()
+            if len(mask_logits) == 0:
+                predictions[current] = (targets[i], 0)
+                continue
             mask_logits = np.clip(mask_logits, self.clip_min, self.clip_max)
             mask_logits = mask_logits / (2 * self.sensitivity / epsilon[i])
 
