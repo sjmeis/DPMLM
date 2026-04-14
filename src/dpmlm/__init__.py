1f196b16800a427540bd8ec7b7a86718270367e8
Stephen Meisenbacher <sjmeis@gtgd.com>
2026-04-14 17:27:40 +0200

    fix import

--- src/dpmlm/__init__.py
+++ src/dpmlm/__init__.py
@@ -1,4 +1,4 @@
 from .core import DPMLM
-from .utils import setup_resources, calculate_logit_bounds
+from .utils import setup_resources, calculate_logit_bounds, cleanup
 
-__all__ = ["DPMLM", "setup_resources", "calculate_logit_bounds"]
+__all__ = ["DPMLM", "setup_resources", "calculate_logit_bounds", "cleanup"]
